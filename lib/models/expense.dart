3b12097b71d7ebb58e68800b3b056f63084ebc9b
Jayrodri088 <johnbetho.c.088@gmail.com>
2024-07-31 23:28:47 +0100

    new

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,11 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
 
 const categoryIcons = {
-  
+  Category.food: Icons.lunch_dining,
+  Category.work: Icons.work,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
 };
 
 class Expense {
@@ -21,4 +28,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  get formattedDate {
+    return formatter.format(date);
+  }
 }
